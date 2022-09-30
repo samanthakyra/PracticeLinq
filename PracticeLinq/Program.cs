3e823ffab57023204a23078fb06526a2dd4decde
samanthakyra <samanthakyramendoza@gmail.com>
2022-09-30 12:22:10 -0700

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<string> videoGames = new()
+            {
+                "COD",
+                "Fall Guys",
+                "Fortnite",
+                "Halo",
+                "Mario Party"
+            };
+videoGames.OrderBy(videoGames => videoGames.Length)
+    .ThenBy(videoGames => videoGames)
+    .ToList()
+    .ForEach(videoGames => Console.WriteLine(videoGames));
